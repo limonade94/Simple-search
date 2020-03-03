bd0db9afa0d78bdbbc4402ab159b19e7a0d49ab9
Phan Chau <pc94200@gmail.com>
2020-03-03 15:11:00 +0100

    Updates

---
+++ precache-manifest.09a0059fe9ffd061789080e54063d119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ed0e92ee3eb46e2c7d0dd244e42541",
+    "url": "/Simple-search/index.html"
+  },
+  {
+    "revision": "86a78e7d02d1afcf3c99",
+    "url": "/Simple-search/static/css/main.61eaf345.chunk.css"
+  },
+  {
+    "revision": "7cac444d062db59bdd23",
+    "url": "/Simple-search/static/js/2.ed8d45af.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Simple-search/static/js/2.ed8d45af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a78e7d02d1afcf3c99",
+    "url": "/Simple-search/static/js/main.00de9974.chunk.js"
+  },
+  {
+    "revision": "b43444e654e86925dfe2",
+    "url": "/Simple-search/static/js/runtime-main.c1622fc0.js"
+  }
+]);
